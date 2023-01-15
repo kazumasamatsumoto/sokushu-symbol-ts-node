040fd01c0b9da08672ef87848ca82469388c5694
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2023-01-15 21:29:09 +0900

    3章完了

--- index.ts
+++ index.ts
@@ -1,4 +1,14 @@
-import { RepositoryFactoryHttp } from "symbol-sdk";
+import {
+  RepositoryFactoryHttp,
+  Account,
+  PublicAccount,
+  Address,
+} from "symbol-sdk";
+const AlicePrivateKey =
+  "B82E003F3DAF29C1E55C39553327B8E178D820396C8A6144AA71329EF391D0EB";
+const AlicePublicKey =
+  "C57096FF4507B39B79F49EB486EBD5E1673B2448974C64231A23CB5BB6E78540";
+const AliceAddress = "TABJ6AP5WNPZF2BEEN2WA6RFK7HR2VCQWXUU6UI";
 
 const example = async (): Promise<void> => {
   // Network information
@@ -11,8 +21,16 @@ const example = async (): Promise<void> => {
   const networkGenerationHash = await repositoryFactory
     .getGenerationHash()
     .toPromise();
-    console.log(epochAdjustment!);
-    console.log(networkType!);
-    console.log(networkGenerationHash!);
+  const accountHttp = repositoryFactory.createAccountRepository();
+  const alice = Address.createFromRawAddress(AliceAddress);
+  accountHttp.getAccountInfo(alice).subscribe(
+    (accountInfo) => {
+      accountInfo.mosaics.forEach((mosaic) => {
+        console.log("id:" + mosaic.id.toHex()); //16進数
+        console.log("amount:" + mosaic.amount.toString()); //文字列
+      });
+    },
+    (err) => console.log(err)
+  );
 };
 example().then();
